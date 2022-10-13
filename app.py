fbb932ef233e8bb4b7d8b305512990ceb5774cef
satinder <satindersingh472@gmail.com>
2022-10-13 16:06:28 -0600

    fixed no items deleted statement

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ def add_candy():
     results = get_display_results('call add_candy(?,?,?)',
     [request.json.get('name'),request.json.get('description'),request.json.get('image_url')])
     if(len(results) == 1):
-        return make_response(json.dumps(f"New Candy has been added to the data, {results[0][0]}",default=str),200) 
+        return make_response(json.dumps(results[0][0],default=str),200) 
     elif(len(results) == 0):
         return make_response(json.dumps("No candy is added",default=str),500)
 
 # delete an existing candy with the help of a id
-
+# and display the results 
 @app.delete('/api/candy')
 def delete_candy():
     invalid = verify_endpoints_info(request.json, ['candy_id'])
@@ -35,11 +35,11 @@ def delete_candy():
     results = get_display_results('call delete_candy(?)',[request.json.get('candy_id')])
     deleted_items = results[0][0]
     if(deleted_items == 0):
-        return "No items deleted, please enter the valid id for candy"
+        return "No candy deleted, please enter the valid id for candy"
     elif(deleted_items == 1):
-        return make_response(json.dumps(deleted_items,default=str),200)
+        return make_response(json.dumps('item deleted',default=str),200)
     else:
-        return "Please check the candy if not deleted then try again."
+        return "There is an error in deleting candy"
 
 if(dbcreds.production_mode == True):
     print('Running in PRODUCTION MODE')
